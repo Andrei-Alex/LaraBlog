9b4a3b524fca7d425f90ec63d88b0d40efcc9ea2
Andrei-Alex <Andrei-Alex@outlook.com>
2024-01-19 10:36:58 +0200

    DebugBar and IDE Helper added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,20 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Post
- *
+ * 
  * Represents a Post entity in the application.
- *
+ * 
  * This class extends Laravel's Eloquent Model and uses the HasFactory trait,
  * enabling it to interact with the database and utilize factories for testing.
- *
+ * 
  * Properties:
+ *
  * @property string $title The title of the post.
  * @property string $slug A slug for the post for URL-friendly identifiers.
  * @property string $content The content/body of the post.
- *
+ * 
  * Attributes:
  * @property array $fillable Attributes that are mass assignable.
  * @property array $guarded Attributes that are not mass assignable.
+ * @mixin IdeHelperPost
  */
 class Post extends Model
 {
