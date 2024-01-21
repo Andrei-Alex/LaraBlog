a1a6111dadd3505cd93ae0581fe45a9cb6c4597a
Andrei <Andrei-Alex@outlook.com>
2024-01-21 21:23:19 +0200

    Docs updated

--- app/Http/Traits/HandleSlug.php
+++ app/Http/Traits/HandleSlug.php
@@ -4,9 +4,24 @@ namespace App\Http\Traits;
 
 use Illuminate\Support\Str;
 
+/**
+ * Trait HandleSlug
+ *
+ * Provides functionality to handle slug generation for models.
+ *
+ * This trait is used to automatically generate a URL-friendly slug. If a slug is provided,
+ * it uses that slug; otherwise, it generates one from the provided title.
+ */
 trait HandleSlug
 {
-    protected function prepareForValidation()
+    /**
+     * Prepare the data for validation and slug generation.
+     *
+     * This method is used to modify the request data before it gets validated.
+     * It ensures that a slug is set for the model. If the slug is not explicitly provided,
+     * it generates one using the title.
+     */
+    protected function prepareForValidation(): void
     {
         $this->merge([
             'slug' => $this->input('slug') ?: Str::slug($this->input('title')),
