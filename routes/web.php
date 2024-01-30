bc90768b36a78a058e2fa214072ec7e04d291474
Andrei <Andrei-Alex@outlook.com>
2024-01-30 19:59:35 +0200

    Next.js Added with breeze-blade and vite

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\PostController;
-use \App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,33 +14,18 @@ use \App\Http\Controllers\AuthController;
 |
 */
 
-/**
- * @return View Returns the welcome view.
- */
 Route::get('/', function () {
     return view('welcome');
 });
 
-/**
- * @return View Returns the Auth Routes .
- */
-Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-Route::post('/login', [AuthController::class, 'auth']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-/**
- * Blog-related routes
- */
-Route::prefix('/blog')->name('blog.')->controller(PostController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'create')->name('create')->middleware('auth');
-    Route::Post('/new', 'store')->name('create')->middleware('auth');
-    Route::get('/{post}/edit', 'edit')->name('edit')->middleware('auth');
-    Route::patch('/{post}/edit', 'update')->name('update')->middleware('auth');
-    Route::get('/{slug}-{post}', 'show')->where([
-        'post' => '[0-9]+',
-        'slug' => '[a-zA-Z0-9\-]+'
-    ])->name('show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+require __DIR__.'/auth.php';
