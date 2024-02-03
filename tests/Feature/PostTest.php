72446ad0d1a10d5b2e9649c26f56b68f86f83cb8
Andrei <Andrei-Alex@outlook.com>
2024-02-03 21:11:42 +0200

    updating tests

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -15,7 +15,7 @@ class postTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_get_list(): void
+    public function test_get_post_list(): void
     {
         $user = User::factory()->create();
         $this->actingAs($user);
@@ -25,5 +25,16 @@ class postTest extends TestCase
         $response = $this->get('/post');
         $response->assertStatus(200);
         $response->assertSee('Blog Post Panel');
+    }
+
+    public function a_post_can_be_soft_deleted()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $post->delete();
+        $this->assertSoftDeleted($post);
+
+        $this->assertTrue(Post::withTrashed()->where('id', $post->id)->exists());
     }
 }
