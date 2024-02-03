4697c10d0995330e66e999485326e60a21002936
Andrei <Andrei-Alex@outlook.com>
2024-02-03 21:02:42 +0200

    updating tests

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,19 +2,28 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use \App\Models\Category;
+use \App\Models\Post;
 
 class postTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_get_list(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        $this->actingAs($user);
 
+        Category::factory()->create();
+        Post::factory()->count(3)->create();
+        $response = $this->get('/post');
         $response->assertStatus(200);
+        $response->assertSee('Blog Post Panel');
     }
 }
