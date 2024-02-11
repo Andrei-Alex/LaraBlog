c1409a3c1f02a1f2cb6abba7cd8919f72d419fee
Andrei <Andrei-Alex@outlook.com>
2024-02-11 18:05:15 +0200

    Updating docs

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -16,17 +16,18 @@ class ViewServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap services.
+     * Bootstrap any application services.
      *
+     * This method is called after all other service providers have been registered,
+     * meaning you have access to all other services that have been registered by the framework.
+     * This is where you can register view composers or share data with views.
      * @return void
      */
     public function boot()
     {
         View::composer('*', function ($view) {
-            // Collect all filter-related request parameters
             $filters = request()->all(['user_id', 'order_by', 'direction']);
 
-            // Share `$filters` with all views
             $view->with('filters', $filters);
         });
     }
