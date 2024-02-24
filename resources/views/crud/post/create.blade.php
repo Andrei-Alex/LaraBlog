38c484bd5c7d3b9bb8b9ed08d51b7c7775831d4a
Andrei <Andrei-Alex@outlook.com>
2024-02-24 22:45:34 +0200

    refactoring

--- resources/views/crud/post/create.blade.php
+++ resources/views/crud/post/create.blade.php
@@ -1,10 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between items-center space-x-4">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __("Create new Post")}}
+        <div class="breadcrumbs-container">
+            <h2>
+                {{ Breadcrumbs::render('post.create') }}
             </h2>
-
         </div>
     </x-slot>
 
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -26,3 +26,15 @@ Breadcrumbs::for('post.show', function ($trail, $post) {
     $trail->parent('post.index');
     $trail->push($post->title, route('post.show', ['slug' => $post->slug, 'post' => $post]));
 });
+
+// Home > Posts > Edit
+Breadcrumbs::for('post.edit', function ($trail, $post) {
+    $trail->parent('post.index');
+    $trail->push($post->title, route('post.edit', ['slug' => $post->slug, 'post' => $post]));
+});
+
+// Home > Posts > New Post
+Breadcrumbs::for('post.create', function ($trail) {
+    $trail->parent('post.index');
+    $trail->push('New Post', route('post.create'));
+});
