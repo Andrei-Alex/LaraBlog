444ae4486af9883099ba776b52be74d5ab18b396
Andrei <Andrei-Alex@outlook.com>
2024-03-12 21:26:02 +0200

    Setup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import '../../styles/globals.css'; // Adjust the path as necessary
 
 const inter = Inter({ subsets: ['latin'] });
 
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      New Page
-    </main>
-  );
+  return <main className="bg-blue-500">New Page</main>;
 }
